63b388de76cdaf718553c2cff33c9794591ccc73
Jesse-js <jesse-jjs@hotmail.com>
2024-01-24 11:36:49 -0300

    feat: add form

---
+++ resources/views/forms/index.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout title="Form Validation">
+    <div class="form mt-5">
+        <form action="">
+            <h5>Form</h5>
+            <hr class="mb-4">
+            <div class="mb-3">
+                <label for="name" class="control-label">Name</label>
+                <input type="text" name="name" id="name" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</x-app-layout>
